fa0195d3885c2769b5a8911dff9191dd9bb64b58
kkuop <60794860+kkuop@users.noreply.github.com>
2020-02-28 16:11:20 -0600

    renamed BinaryTree class to CustomBinaryTree

---
+++ BinaryTreeSearch/BinaryTrees/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeSearch.BinaryTree
+{
+    class Node
+    {
+        //member vars
+        public string key;
+        public int data;
+        public Node left;
+        public Node right;
+        public bool isLeaf;
+        //constructor
+        public Node(string key, int data, Node left, Node right, bool isLeaf)
+        {
+            this.key = key;
+            this.data = data;
+            this.left = left;
+            this.right = right;
+            this.isLeaf = isLeaf;
+        }
+        //member methods
+        
+    }
+}
